4c6597df68be3f23795a26150c3911e2f4a8be37
prakritipi <prakritiacharyaa@gmail.com>
2025-07-24 13:12:18 +0545

    lit

--- src/Components/UM_setup/Childupdate.tsx
+++ src/Components/UM_setup/Childupdate.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
 const Childupdate = ({ data, onUpdate }) => {
-  const handleChangeLabel = (id: React.Key | null | undefined) => {
-    const updatedData = data.map((item: { id: React.Key | null | undefined; }) =>
-      item.id === id ? { ...item, label: "items added!!!" } : item
+  const handleChangeLabel = (id: any) => {
+    const updatedData = data.map((item: { id: any; }) =>
+      item.id === id ? { ...item, label: "Added!!!" } : item
     );
     onUpdate(updatedData); 
   };
@@ -12,11 +12,11 @@ const Childupdate = ({ data, onUpdate }) => {
     <div>
       <h3>Child Component</h3>
       <ul>
-        {data.map((item: { id: React.Key | null | undefined; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; label: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
+        {data.map((item: { id: Key | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; label: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => (
           <li key={item.id}>
             {item.name} - {item.label}
             <button onClick={() => handleChangeLabel(item.id)} style={{ marginLeft: "10px" }}>
-              items
+              Change Label
             </button>
           </li>
         ))}
